59c2f5fd03bfa1be917486d41806323ed41c396b
Shadab Alam <shadab19it@gmail.com>
2024-03-23 15:53:24 +0530

    make sider collapsable

--- src/components/SvgIcons.tsx
+++ src/components/SvgIcons.tsx
@@ -358,4 +358,33 @@ export default {
       <path d="M4.5 4.5a3 3 0 0 0-3 3v9a3 3 0 0 0 3 3h8.25a3 3 0 0 0 3-3v-9a3 3 0 0 0-3-3H4.5ZM19.94 18.75l-2.69-2.69V7.94l2.69-2.69c.944-.945 2.56-.276 2.56 1.06v11.38c0 1.336-1.616 2.005-2.56 1.06Z" />
     </svg>
   ),
+
+  carretLeft: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      height={20}
+      width={20}
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+    </svg>
+  ),
+  carretRight: (
+    <svg
+      height={20}
+      width={20}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+    </svg>
+  ),
 };
