468a90c3950e19b775c86860384ff08573dddff1
mehrab-alam <mehrab2000alam@gmail.com>
2024-05-04 11:55:27 +0530

    fixed the prisma bug

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NEXT_PUBLIC_APP_ENV === "development",
+  disable: process.env.NODE_ENV === "development",
 });
 
 module.exports = withPWA(nextConfig);
