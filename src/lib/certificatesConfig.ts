9cc3ee2d588b0dc414264a04fc5048a61b98ca7d
Shad <shad.amezng@gmail.com>
2024-05-13 15:25:30 +0530

    added certificate templates (#149)

---
+++ src/lib/certificatesConfig.ts
@@ -0,0 +1,46 @@
+export const certificateConfig = [
+  {
+    id: "golden-luxury",
+    path: "/img/certificates/templates/golden-luxury.png",
+    coordinates: {
+      student: {
+        x: 1000,
+        y: 700,
+      },
+      description: {
+        x: 1000,
+        y: 900,
+      },
+      dateOfCompletion: {
+        x: 545,
+        y: 1070,
+      },
+      authorSignature: {
+        x: 1450,
+        y: 1070,
+      },
+    },
+  },
+  {
+    id: "golden-standard",
+    path: "/img/certificates/templates/golden-standard.png",
+    coordinates: {
+      student: {
+        x: 1000,
+        y: 630,
+      },
+      description: {
+        x: 1000,
+        y: 780,
+      },
+      dateOfCompletion: {
+        x: 544,
+        y: 1099,
+      },
+      authorSignature: {
+        x: 1446,
+        y: 1099,
+      },
+    },
+  },
+];
