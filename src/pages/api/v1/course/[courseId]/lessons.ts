df67056cc4bc6c88e8dd036faf20b89c17426063
Shad <shad.amezng@gmail.com>
2024-05-16 22:38:13 +0530

    added the query for fetching course lessons

--- src/pages/api/v1/course/[courseId]/lessons.ts
+++ src/pages/api/v1/course/[courseId]/lessons.ts
@@ -3,10 +3,62 @@ import { NextApiResponse, NextApiRequest } from "next";
 import { errorHandler } from "@/lib/api-middlewares/errorHandler";
 import { withMethods } from "@/lib/api-middlewares/with-method";
 import { withAuthentication } from "@/lib/api-middlewares/with-authentication";
+import { getCookieName } from "@/lib/utils";
+import { getToken } from "next-auth/jwt";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const { courseId } = req.query;
+
+    let cookieName = getCookieName();
+
+    const token = await getToken({
+      req,
+      secret: process.env.NEXT_PUBLIC_SECRET,
+      cookieName,
+    });
+
+    const resultRows = await prisma.$queryRaw<
+      any[]
+    >`SELECT  ch.sequenceId as chapterSeq, re.sequenceId as resourceSeq, re.name as lessonName, vi.id as videoId, vi.videoUrl, ch.chapterId, 
+    ch.name as chapterName, cp.resourceId as watchedRes FROM testconsole.Course as co 
+   INNER JOIN testconsole.CourseRegistration as cr ON co.courseId = cr.courseId
+   INNER JOIN testconsole.Chapter as ch ON co.courseId = ch.courseId
+   INNER JOIN testconsole.Resource as re ON ch.chapterId = re.chapterId
+   INNER JOIN testconsole.Video as vi ON re.resourceId = vi.resourceId
+   LEFT OUTER JOIN testconsole.CourseProgress as cp ON cp.user_id = cr.studentId AND cp.resourceId = re.resourceId AND cp.chapterId = ch.chapterId
+   WHERE cr.studentId = ${token?.id}
+   AND co.courseId = ${courseId}
+   ORDER BY chapterSeq, resourceSeq`;
+
+    let chapterLessons: any[] = [];
+    resultRows.forEach((r) => {
+      if (chapterLessons.find((l) => l.chapterSeq == r.chapterSeq)) {
+        const chapter = chapterLessons.find((l) => l.chapterSeq == r.chapterSeq);
+        chapter.lessons.push({
+          videoId: r.videoId,
+          title: r.lessonName,
+          videoUrl: r.videoUrl,
+          isWatched: r.watchedRes != null,
+        });
+      } else {
+        chapterLessons.push({
+          chapterSeq: r.chapterSeq,
+          chapterName: r.chapterName,
+          lessons: [
+            {
+              videoId: r.videoId,
+              title: r.lessonName,
+              videoUrl: r.videoUrl,
+              isWatched: r.watchedRes != null,
+            },
+          ],
+        });
+      }
+    });
+
+    console.log(chapterLessons);
+
     const course = await prisma.course.findUnique({
       where: {
         courseId: Number(courseId),
@@ -31,7 +83,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       success: true,
       statusCode: 200,
       message: "Fetched course lessons",
-      courseDetails: course,
+      lessons: chapterLessons,
     });
   } catch (error) {
     return errorHandler(error, res);
