af874a1d883bd2803d61aa3f8005c07e069fd709
mehrab-alam <mehrab2000alam@gmail.com>
2024-06-11 17:43:43 +0530

    fixed: empty comment fetched while route via notification url

--- src/pages/api/v1/discussions/get/[cmtId].ts
+++ src/pages/api/v1/discussions/get/[cmtId].ts
@@ -7,14 +7,34 @@ import { errorHandler } from "@/lib/api-middlewares/errorHandler";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const commentId = req.query?.cmtId;
-    const comment = await prisma.discussion.findFirst({
-      where: {
-        id: Number(commentId),
-      },
-      include: {
-        user: true,
-      },
+
+    const rawData = await prisma.$queryRaw<any[]>`
+    select dis.id, dis.userId, name, image,  comment, dis.createdAt, 
+COUNT(thread.reply_id) as replyCount FROM Discussion as dis
+INNER JOIN User ON dis.userId = User.id
+LEFT OUTER JOIN ( SELECT id as reply_id, parentCommentId FROM Discussion) as thread
+ON dis.id = thread.parentCommentId
+WHERE dis.parentCommentId is NULL AND dis.id =${Number(commentId)}
+GROUP BY dis.id, userId, comment, dis.createdAt
+
+`;
+
+    const comment = rawData.map((data) => {
+      return {
+        ...data,
+        comment: data.comment,
+        id: data.id,
+
+        user: {
+          id: data.userId,
+          name: data.name,
+          image: data.image,
+        },
+        createdAt: data.createdAt,
+        replyCount: Number(data.replyCount),
+      };
     });
+
     return res.status(200).json({ success: true, comment, messsage: "Comment Found" });
   } catch (err) {
     return errorHandler(err, res);
