724c4d0156ebe26460cb1c5c285fa0f40e51f78a
mehrab-alam <mehrab2000alam@gmail.com>
2024-06-15 12:28:17 +0530

    chore: Removed unused package.json dependencies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,31 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import "../styles/globalStyles.scss";
 import "antd/dist/reset.css";
-import { SessionProvider, signOut } from "next-auth/react";
+import { SessionProvider } from "next-auth/react";
 import { Session } from "next-auth";
-import { Button, ConfigProvider } from "antd";
 import NextNProgress from "nextjs-progressbar";
-import antThemeConfig from "@/services/antThemeConfig";
 import { AppProvider } from "@/components/ContextApi/AppContext";
-import darkThemConfig from "@/services/darkThemConfig";
 
 function App({ Component, pageProps }: AppProps<{ session: Session }>) {
-  const [enabledDarkTheme, setCurrentTheme] = useState(false);
-
   return (
     <>
-      <AppProvider
-        themeSwitcher={() => {
-          // console.log(pageProps.session);
-          // let mainHTML = document.getElementsByTagName("html").item(0);
-          // if (mainHTML != null) {
-          //   mainHTML.setAttribute("data-theme", enabledDarkTheme ? "light" : "dark");
-          // }
-          // setCurrentTheme(!enabledDarkTheme);
-        }}
-      >
+      <AppProvider>
         <SessionProvider session={pageProps.session}>
           <NextNProgress />
           <Component {...pageProps} />
