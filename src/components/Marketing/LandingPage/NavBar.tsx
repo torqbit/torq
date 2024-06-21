66bb880a7200497628a2bef889fffa188a81a2d7
Shad <shad.amezng@gmail.com>
2024-06-21 19:14:28 +0530

    added navbar dropdown for landing page

--- src/components/Marketing/LandingPage/NavBar.tsx
+++ src/components/Marketing/LandingPage/NavBar.tsx
@@ -1,11 +1,12 @@
 import { FC } from "react";
 import styles from "@/styles/NavBar.module.scss";
 import Image from "next/image";
-import { Button, Flex, Tooltip } from "antd";
+import { Button, Dropdown, Flex, MenuProps, Space, Tooltip } from "antd";
 import appConstant from "@/services/appConstant";
 import Link from "next/link";
 import SvgIcons from "@/components/SvgIcons";
 import { useAppContext } from "@/components/ContextApi/AppContext";
+import { DownOutlined } from "@ant-design/icons";
 const NavBar: FC<{}> = () => {
   const { dispatch } = useAppContext();
 
@@ -44,6 +45,60 @@ const NavBar: FC<{}> = () => {
       });
     }
   };
+
+  const items: MenuProps["items"] = [
+    {
+      key: "1",
+      type: "group",
+      label: "Front end Development",
+      children: [
+        {
+          key: "1-1",
+          label: "Foundations of Web Development",
+        },
+        {
+          key: "1-2",
+          label: "Code Collaboration with Git & Github",
+        },
+        {
+          key: "1-3",
+          label: "Code Collaboration with Git & Github",
+        },
+        {
+          key: "1-4",
+          label: "Code Collaboration with Git & Github",
+        },
+      ],
+    },
+    {
+      key: "1",
+      type: "group",
+      label: "Back end Development",
+      children: [
+        {
+          key: "2-1",
+          label: "Foundations of Web Development",
+        },
+        {
+          key: "2-2",
+          label: "Code Collaboration with Git & Github",
+        },
+        {
+          key: "2-3",
+          label: "Code Collaboration with Git & Github",
+        },
+        {
+          key: "2-4",
+          label: "Code Collaboration with Git & Github",
+        },
+      ],
+    },
+    {
+      key: "4",
+      danger: true,
+      label: "a danger item",
+    },
+  ];
   return (
     <div className={styles.navBarContainer}>
       <nav>
@@ -57,7 +112,20 @@ const NavBar: FC<{}> = () => {
           {navLinks.map((link, i) => {
             return (
               <li key={i}>
-                <Link href={link.href}>{link.title}</Link>
+                <Dropdown
+                  menu={{ items }}
+                  trigger={["click"]}
+                  overlayClassName="nav__overlay"
+                  className="another__class"
+                >
+                  <a onClick={(e) => e.preventDefault()}>
+                    <Space>
+                      {link.title}
+                      <DownOutlined />
+                    </Space>
+                  </a>
+                </Dropdown>
+                {/* <Link href={link.href}>{link.title}</Link> */}
               </li>
             );
           })}
