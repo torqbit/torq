ddfa7b55d53076e751c1f71b2bb73bf3f8bbddd7
Mehrab <122259451+mehrab-alam@users.noreply.github.com>
2024-07-15 15:17:36 +0530

    feat: made certificate page responsive (#350)

--- src/components/SvgIcons.tsx
+++ src/components/SvgIcons.tsx
@@ -34,6 +34,18 @@ export default {
       <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
     </svg>
   ),
+  arrowLeft: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-6"
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+    </svg>
+  ),
   imgUpload: (
     <svg
       xmlns="http://www.w3.org/2000/svg"
